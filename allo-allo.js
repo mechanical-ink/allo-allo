1a5a8d14ab5511fb5cbc3bdff3ec6a6f95259b0e
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-05-07 21:22:07 +0200

    create comment

--- allo-allo.js
+++ allo-allo.js
@@ -2,12 +2,21 @@ const github = require("@actions/github");
 // const core = require("@actions/core");
 
 async function alloAllo() {
-  // const token = core.getInput("GH_TOKEN");
-  // const octokit = new github.getOctokit(token);
-  // const issueComment = core.getInput("issueGreeting");
+  const token = core.getInput("GH_TOKEN");
+  const octokit = new github.getOctokit(token);
+  const issueComment = core.getInput("issueGreeting");
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  return payload;
+
+  const response = octokit.rest.issues.createComment({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.html_url,
+    issue_number: payload.issue.number,
+    body: issueComment,
+  });
+
+  return response;
 }
 
 module.exports = alloAllo;
