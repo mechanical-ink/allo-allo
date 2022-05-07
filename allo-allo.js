1172d6c328dd0d67df9f61ac0daf943173406405
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-05-07 21:30:08 +0200

    add logging

--- allo-allo.js
+++ allo-allo.js
@@ -8,6 +8,7 @@ async function alloAllo() {
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log("payload", payload);
 
   const response = octokit.rest.issues.createComment({
     owner: payload.repository.owner.login,
